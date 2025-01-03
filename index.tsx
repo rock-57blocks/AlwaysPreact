76221d51ecb66a876e54e4248e8598eed050b61e
rock-57blocks <rchen@ext.liftoff.io>
2025-01-03 10:55:28 +0800

    aa

--- index.tsx
+++ index.tsx
@@ -1,11 +1,8 @@
 import { render } from "preact";
-import TestPlayer, { muteVideo } from "./src/test-player";
+import TestPlayer from "./src/test-player";
 
 const bootstrap = () => {
     render(<TestPlayer />, document.getElementById('app')!);
-    window.setTimeout(() => {
-        muteVideo.value = true;
-    }, 3000);
 };
 
 bootstrap();
--- src/test-player.tsx
+++ src/test-player.tsx
@@ -1,46 +1,30 @@
-import { Fragment, FunctionalComponent } from 'preact';
+import { FunctionalComponent } from 'preact';
 import { useEffect, useRef } from 'preact/hooks';
-import { useSignalEffect, useSignal, signal } from '@preact/signals';
-
-
-type MuteButtonProps = {
-  isMuted: boolean;
-};
-
-const MuteButton: FunctionalComponent<MuteButtonProps> = ({
-  isMuted,
-}) => {
-  useEffect(() => {
-    console.log('isMuted', isMuted);
-  }, [isMuted]);
-
-  return null;
-};
-
-export const muteVideo = signal(false);
+import { useSignalEffect, useSignal } from '@preact/signals';
 
 const TestPlayer: FunctionalComponent = () => {
-  const divRef = useRef<HTMLDivElement>(null);
   const videoRef = useRef<HTMLVideoElement>(null);
+  const hlsUrl = useSignal("");
+
+  useEffect(() => {
+    const getHlsUrl = async () => {
+      // this is the key issue here, if there is a promise in the useEffect, the test will fail
+      const url = await new Promise<string>(resolve => {
+        resolve("https://hlsurl.com/");
+      });
+      console.log("url changed", url);
+      hlsUrl.value = url;
+    };
+
+    getHlsUrl();
+  }, []);
+
   useSignalEffect(() => {
-    videoRef.current!.muted = muteVideo.value;
-    divRef.current!.innerText = muteVideo.value ? 'Muted Should be right' : 'Unmuted is wrong';
+    console.log("call useSignalEffect with hlsUrl.value", hlsUrl.value);
+    videoRef.current!.src = hlsUrl.value;
   });
 
-  return (
-    <Fragment>
-      <div>
-        <div ref={divRef}></div>
-        <video
-          data-testid='video-player'
-          ref={videoRef}>
-        </video>
-        <MuteButton
-          isMuted={muteVideo.value}
-        />
-      </div>
-    </Fragment>
-  );
-};
+  return <video data-testid="video-player" ref={videoRef}></video>;
+}
 
 export default TestPlayer;
